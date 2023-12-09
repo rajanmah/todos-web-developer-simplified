50beb94ff9bca19dcc73d9e79fb2d5a28243eed0
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-12-08 19:17:46 -0500

    code along web dev simplified

---
+++ src/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ACTIONS } from './App.jsx'
+
+export default function Todo({ todo, dispatch }) {
+    return (
+        <div>
+            <span style={{ color: todo.complete ? 'red' : '#000' }}>{todo.name}</span>
+            <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
+            <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+
+
+        </div>
+    )
+}
+
